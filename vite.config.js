a7fdee481f581772f2bc919509c1d29defabc946
abin <wellxabin@gmail.com>
2026-03-26 15:39:09 +0800

    部署

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages 子路径部署时可改为 '/仓库名/'
-  base: './',
+  base: '/prompt-assembly/'
 })
